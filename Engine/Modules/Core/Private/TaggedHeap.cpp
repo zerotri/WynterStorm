a5371df187d945661036c2e31e8c67705d60e60c
Wynter Woods <zerotri@gmail.com>
2020-11-25 09:54:34 -0800

    Tagged Heap Allocator: - Set a fixed base memory address - Fixed a bug with calculation of allocated block addresses
    Signed-off-by: Wynter Woods <zerotri@gmail.com>

--- Engine/Modules/Core/Private/TaggedHeap.cpp
+++ Engine/Modules/Core/Private/TaggedHeap.cpp
@@ -2,6 +2,8 @@
 #include <cstring>
 #include <cstdint>
 #include <sys/mman.h>
+#include <stdio.h>
+#include <inttypes.h>
 
 #ifdef __APPLE__
 #define PLATFORM_HAS_MMAP 1
@@ -17,6 +19,7 @@
 #define PLATFORM_HAS_VIRTUALALLOC 0
 #endif
 
+constexpr uintptr_t tagged_heap_fixed_addr          = 0x200000000;
 constexpr size_t tagged_heap_size                   = 0x40000000;   // 1GB
 constexpr size_t tagged_heap_block_size             =   0x100000;   // 1MB
 constexpr size_t tagged_heap_block_count            = tagged_heap_size / tagged_heap_block_size;
@@ -32,7 +35,7 @@ int ws_tagged_heap_init()
 {
 #if PLATFORM_HAS_MMAP == 1
     // allocate large memory block
-    ws_tagged_heap_ptr = (ws_tagged_heap_t*) mmap(0, tagged_heap_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    ws_tagged_heap_ptr = (ws_tagged_heap_t*) mmap((void*)tagged_heap_fixed_addr, tagged_heap_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     
     if(ws_tagged_heap_ptr == MAP_FAILED)
     {
@@ -68,7 +71,11 @@ void* ws_tagged_heap_alloc_block(ws_tag_t tag)
         if(ws_tagged_heap_ptr->block_tags[i] == 0)
         {
             ws_tagged_heap_ptr->block_tags[i] = tag;
-            void* block_address = ws_tagged_heap_ptr + i * tagged_heap_block_size;
+            void* block_address = (void*)((uintptr_t) ws_tagged_heap_ptr + (uintptr_t) i * (uintptr_t)tagged_heap_block_size);
+#if WS_DEBUG == 1
+            printf("[ws_tagged_heap_alloc_block] a: %" PRIxPTR" p: %" PRIxPTR" i: %" PRIxPTR" s: %" PRIxPTR" \n", 
+                (uintptr_t) block_address, (uintptr_t) ws_tagged_heap_ptr, (uintptr_t) i, (uintptr_t) tagged_heap_block_size);
+#endif
             return block_address;
         }
     }
@@ -107,7 +114,7 @@ void* ws_tagged_heap_alloc_n_blocks(ws_tag_t tag, size_t blocks)
                     tags[i] = tag;
                 }
 
-                void* block_address = ws_tagged_heap_ptr + start * tagged_heap_block_size;
+                void* block_address = (void*)((uintptr_t) ws_tagged_heap_ptr + (uintptr_t) start * (uintptr_t)tagged_heap_block_size);
                 return block_address;
             }
         }
